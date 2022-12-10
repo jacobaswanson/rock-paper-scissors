625b2c62abe01803c9b5c7fcaf941b6d194bb1ad
jacobaswanson <jakeaswan@yahoo.com>
2022-12-09 23:34:51 -0600

    Added a function for creating a computers choice

--- script.js
+++ script.js
@@ -13,4 +13,43 @@ function getUserChoice(){
 }
 
 function playRound(){
+    const userChoice = getUserChoice();
+    const computerChoice = getComputerChoice();
+    let result = 2;
+    if(userChoice === 'rock'){
+        if(computerChoice === 'rock'){
+            result = 2;
+        }else if(computerChoice === 'paper'){
+            result = 0;
+        }else if(computerChoice === 'scissors'){
+            result = 1;
+        }
+    }else if(userChoice === 'paper'){
+        if(computerChoice === 'rock'){
+            result = 1;
+        }else if(computerChoice === 'paper'){
+            result = 2;
+        }else if(computerChoice === 'scissors'){
+            result = 0;
+        }
+    }else if(userChoice === 'scissors'){
+        if(computerChoice === 'rock'){
+            result = 0;
+        }else if(computerChoice === 'paper'){
+            result = 1;
+        }else if(computerChoice === 'scissors'){
+            result = 2;
+        }
+    }
+}
+
+function game(){
+    for(let i = 0; i < 5; i++){
+        let roundResult = playRound();
+        let wins = 0;
+        let losses = 0;
+        if(roundResult = 0){
+            
+        }
+    }
 }
